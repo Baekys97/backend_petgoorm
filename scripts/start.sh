bfd2d83e4a5469e344b552f778e38a30908bb86a
Baekys97 <117141532+Baekys97@users.noreply.github.com>
2023-10-28 01:55:03 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,7 +15,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # redis container 실행
 echo "> Start Redis Container"
-docker-compose -f $REPOSITORY/docker-compose-redis.yml up -d --build redis
+docker-compose -f $PROJECT_ROOT/docker-compose-redis.yml up -d redis
 
 echo "> Remove dangling images"
 docker rmi -f $(docker images -f "dangling=true" -q) || true
