f6161fcad0a91c68d52a37608dc2a3128e6ed671
Baekys97 <117141532+Baekys97@users.noreply.github.com>
2023-10-27 22:58:56 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -20,3 +20,9 @@ fi
 
 echo "> Deploy - $JAR_PATH "
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+echo "> Start Redis Container"
+docker-compose -f $REPOSITORY/docker-compose-redis.yml up -d redis
+
+echo "> Remove dangling images"
+docker rmi -f $(docker images -f "dangling=true" -q) || true
